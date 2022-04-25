81b5d4f38447f80cd039a0679212564039706606
Juan <juanguti967@gmail.com>
2022-04-25 16:44:55 -0500

    Creación v2

--- Ejercicio_POO_1.py
+++ Ejercicio_POO_1.py
@@ -31,6 +31,7 @@ class Empresa:
 
 hector = Cliente(nombre="Hector",apellidos="Costa Guzman", dni="111111A")
 juan = Cliente("222222B","Juan","Gonzales Marquez")
+daniel = Cliente("111111C","Daniel","Almario Quintero")
 empresa= Empresa(clientes=[hector,juan])
 empresa.mostrar_cliente("222222B")
 empresa.borrar_cliente("222222B")
