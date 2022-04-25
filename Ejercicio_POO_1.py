543fa3d13a1317aa062ef05663ea0fa81577b5d9
Juan <juanguti967@gmail.com>
2022-04-25 16:35:18 -0500

    Creación v1

---
+++ Ejercicio_POO_1.py
@@ -0,0 +1,37 @@
+class Cliente:
+    
+    def __init__(self, dni, nombre, apellidos):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellidos = apellidos
+        
+    def __str__(self):
+        return '{} {} melo'.format(self.nombre,self.apellidos)
+    
+
+class Empresa:
+    
+    def __init__(self, clientes=[]):
+        self.clientes = clientes
+        
+    def mostrar_cliente(self, dni=None):
+        for c in self.clientes:
+            if c.dni == dni:
+                print(c)
+                return
+        print("Cliente no encontrado")
+    
+    def borrar_cliente(self, dni=None):
+        for i,c in enumerate(self.clientes):
+            if c.dni == dni:
+                del(self.clientes[i])
+                print(c,"> BORRADO")
+                return
+        print("Cliente no encontrado")
+
+hector = Cliente(nombre="Hector",apellidos="Costa Guzman", dni="111111A")
+juan = Cliente("222222B","Juan","Gonzales Marquez")
+empresa= Empresa(clientes=[hector,juan])
+empresa.mostrar_cliente("222222B")
+empresa.borrar_cliente("222222B")
+print(str(juan))
